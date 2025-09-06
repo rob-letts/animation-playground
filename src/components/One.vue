39fda00caf22d14dcd85af131060c1641ffbb9ae
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2025-09-06 17:23:15 +0100

    some placeholder animation for #1

--- src/components/One.vue
+++ src/components/One.vue
@@ -1,9 +1,48 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { gsap } from 'gsap'
+
+const words = ref('You should hover over me.')
+
+function activate(event: MouseEvent) {
+	const target = event.target as HTMLElement
+
+	gsap.to(target, {
+		scale: 1.5,
+		color: 'red',
+		duration: 0.3,
+		ease: 'power1.out',
+	})
+	gsap.to(target, {
+		scale: 1,
+		color: 'white',
+		duration: 0.3,
+		ease: 'power1.out',
+		delay: 0.3,
+	})
+}
 </script>
 
 <template>
-  <h2>Animation Playground One</h2>
+  <h2>
+    <span
+      v-for="char, index in words"
+      @mouseover="activate"
+      :key="index"
+      :class="{ space: char === ' ' }"
+      class="char"
+    >
+      {{ char }}
+    </span>
+  </h2>
 </template>
 
-<style scoped></style>
+<style scoped>
+.char {
+  display: inline-block;
+}
+
+.space {
+  margin-right: 1rem;
+}
+</style>
